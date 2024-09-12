bd4602bf30c1bff9b4d8a1116ca092e99ae25fb0
Sean McCarty <soxfansean@gmail.com>
2024-09-12 17:40:47 -0400

    Edit tests

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,5 @@
 ## Please fill in all the parts labeled as ### YOUR CODE HERE
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cs
 import numpy as np
 import pytest
 from utils import *
@@ -18,13 +18,13 @@ def test_cosine_similarity():
     
     dp = dot_product(vector1, vector2)
     result = dp / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
-    expected_result = cosine_similarity(vector1, vector2)
+    expected_result = cs(vector1, vector2)
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
     vector1 = np.array([1,2])
-    matrix1 = np.matrix('2 4; 3 2; 1 2')
+    matrix1 = np.array([[3,2], [4,3], [1,2]])
     
     
     result = nearest_neighbor(vector1, matrix1)
